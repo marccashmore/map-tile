1f687e9647cb2b440f396d1f3f0f7679d9a9251b
marccashmore <marc.cashmore@gmail.com>
2024-05-03 13:55:44 +0100

    modified:   index.html modified:   maptile.js

--- maptile.js
+++ maptile.js
@@ -1,1 +1,14 @@
-document.getElementById("demo").innerHTML = "Hello JavaScript!";
+function getTile() {
+  var x_val = document.getElementById("x_val").value;
+  var y_val = document.getElementById("y_val").value;
+  var zoom = document.getElementById("zoom").value;
+
+  var tileURL = "https://tile.openstreetmap.org/" + zoom + "/" + x_val + "/" + y_val + ".png"
+  document.getElementById("tileId").innerHTML = tileURL;
+
+  src = tileURL,
+  img = document.createElement('img');
+
+  img.src = src;
+  document.body.appendChild(img);
+}
