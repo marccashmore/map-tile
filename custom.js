19f3904fe1295a701508546d4a13f7174fd4dcd6
etiologist <65084030+etiologist@users.noreply.github.com>
2024-05-07 16:40:38 +0100

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,25 @@
+(function () {
+    "use strict";
+    'use strict';
+
+
+    var app = angular.module('viewCustom', ['angularLoad']);
+
+    /****************************************************************************************************/
+
+        /*In case of CENTRAL_PACKAGE - comment out the below line to replace the other module definition*/
+
+        /*var app = angular.module('centralCustom', ['angularLoad']);*/
+
+    /****************************************************************************************************/
+
+app.component('prmSearchBarAfter', {
+        template: `<span style="margin-left: 40%;">Hello World</span>`
+ 
+    });	
+
+})
+
+
+
+   
